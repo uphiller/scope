2ebb4d22a3bc6107b4bec66788acb717c186aad6
Bryan Boreham <bjboreham@gmail.com>
2018-10-29 13:00:01 +0000

    Update typetypetype/conntrack package
    For perf improvement and panic avoidance.

--- vendor/github.com/typetypetype/conntrack/client.go
+++ vendor/github.com/typetypetype/conntrack/client.go
@@ -147,8 +147,10 @@ func Follow(flags uint32) (<-chan Conn, func(), error) {
 
 // Follow gives a channel with all changes, , using specified netlink buffer size.
 func FollowSize(bufferSize int, flags uint32) (<-chan Conn, func(), error) {
+	var closing bool
 	s, _, err := connectNetfilter(bufferSize, flags)
 	stop := func() {
+		closing = true
 		syscall.Close(s)
 	}
 	if err != nil {
@@ -164,7 +166,7 @@ func FollowSize(bufferSize int, flags uint32) (<-chan Conn, func(), error) {
 			// continue
 			// }
 			res <- c
-		}); err != nil {
+		}); err != nil && !closing {
 			panic(err)
 		}
 	}()
@@ -303,7 +305,8 @@ func (c Conn) ConnTCP(local map[string]struct{}) *ConnTCP {
 func parsePayload(b []byte) (*Conn, error) {
 	// Most of this comes from libnetfilter_conntrack/src/conntrack/parse_mnl.c
 	conn := &Conn{}
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return conn, err
 	}
@@ -333,7 +336,8 @@ func parsePayload(b []byte) (*Conn, error) {
 }
 
 func parseTuple(b []byte, tuple *Tuple) error {
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return fmt.Errorf("invalid tuple attr: %s", err)
 	}
@@ -356,7 +360,8 @@ func parseTuple(b []byte, tuple *Tuple) error {
 }
 
 func parseCounters(b []byte) (uint64, uint64, error) {
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return 0, 0, fmt.Errorf("invalid tuple attr: %s", err)
 	}
@@ -374,7 +379,8 @@ func parseCounters(b []byte) (uint64, uint64, error) {
 }
 
 func parseIP(b []byte, tuple *Tuple) error {
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return fmt.Errorf("invalid tuple attr: %s", err)
 	}
@@ -396,7 +402,8 @@ func parseIP(b []byte, tuple *Tuple) error {
 }
 
 func parseProto(b []byte, tuple *Tuple) error {
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return fmt.Errorf("invalid tuple attr: %s", err)
 	}
@@ -427,7 +434,8 @@ func parseProto(b []byte, tuple *Tuple) error {
 }
 
 func parseProtoinfo(b []byte, conn *Conn) error {
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return fmt.Errorf("invalid tuple attr: %s", err)
 	}
@@ -445,7 +453,8 @@ func parseProtoinfo(b []byte, conn *Conn) error {
 }
 
 func parseProtoinfoTCP(b []byte, conn *Conn) error {
-	attrs, err := parseAttrs(b)
+	var attrSpace [16]Attr
+	attrs, err := parseAttrs(b, attrSpace[0:0])
 	if err != nil {
 		return fmt.Errorf("invalid tuple attr: %s", err)
 	}
